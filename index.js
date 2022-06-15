7df2680e8251cf6fdd42cd2a4482bb0af80da483
tufpsj <tufpsj@yonyou.com>
2022-06-15 09:54:03 +0800

    # 修复一下BUG

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+#! /usr/bin/env node
+
 const program = require("commander");
 const creator = require("./lib/creator");
 
-program
-    .version('0.1.0')
-    .option('-c , --context  [context]', 'context path')
-    .usage('create app-name').arguments('create app-name')
-    .action(function (cmd, env, p) {
-        if (cmd === "create") {
-            creator(env, p.context);
-        }
-    });
+function main() {
+    program.version('0.1.8');
+    program.usage('create app-name [options]');
+    program.command('create <app-name>')
+        .option('-c, --context <context>', 'front-end context path')
+        .option('-a, --author <author>', 'front-end project creator')
+        .option('-e, --email <email>', 'front-end project creator email')
+        .option('-v, --version <version>', 'front-end project version')
+        .option('-p, --port <port>', 'front-end project listene port')
+        .action(function (appName, options) {
+            creator(appName, options);
+        });
 
-program.parse(process.argv);
+    program.description('create a8 project from base template');
+    program.parse(process.argv);
+}
+
+if (require.main === module) {
+    main();
+}
