ddbf51a6de33dd04df38547eea0bcadc88fc8ca6
tufpsj <tufpsj@yonyou.com>
2022-06-16 19:29:28 +0800

    + 加入模块开发模板支持

--- README.md
+++ README.md
@@ -4,10 +4,23 @@ A8 产品前端环境脚手架，通过命令可以快速构建A8前端工程。
 
 ## HOW TO USE
 
+### PROJECT
+
 1. install the package `npm install -g aip-toolkit`
 2. create a8 project `npx aip-toolkit create <project-name> -c <context-path>`
 3. cd project-name directory and run `npm i && npm start`
 
+
 ~~~sh
 npx aip-toolkit create helloworld -c "hello"
+~~~
+
+### Module
+
+1. install the package `npm install -g aip-toolkit`
+2. create a8 project `npx aip-toolkit module <module-name>`
+3. cd project-name directory and run `./buildandpublish.sh`
+
+~~~sh
+npx aip-toolkit module helloworld
 ~~~
--- index.js
+++ index.js
@@ -13,10 +13,18 @@ function main() {
         .option('-v, --version <version>', 'front-end project version')
         .option('-p, --port <port>', 'front-end project listene port')
         .action(function (appName, options) {
-            creator(appName, options);
+            creator(appName, options, false);
         });
 
-    program.description('create a8 project from base template');
+    program.command('module <module-name>')
+        .option('-a, --author <author>', 'front-end project creator')
+        .option('-e, --email <email>', 'front-end project creator email')
+        .option('-v, --version <version>', 'front-end project version')
+        .action(function (appName, options) {
+            creator(appName, options, true);
+        });
+
+    program.description('create a8 project/aip module from base template');
     program.parse(process.argv);
 }
 
