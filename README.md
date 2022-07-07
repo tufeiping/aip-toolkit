5e27475e3495033af08c26d2bb2b9da06060dd46
tufeiping <tufp@yonyousj.com>
2022-07-08 07:28:48 +0800

    + 加入rich-client构建方式

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-## AIP-ToolKit
+## A8前端工程创建脚手架 (AIP-ToolKit)
 
 A8 产品前端环境脚手架，通过命令可以快速构建A8前端工程。
 
+A8的项目在核心依赖上有其特定要求，为了不让研发人员自己去定义项目的核心依赖，造成版本冲突及浪费不必要的调试时间，A8提供了一个脚手架，可以让研发人员在规定版本范围快速进入开发阶段。
+
+
 ## HOW TO USE
 
-### PROJECT
+### Project
 
 1. install the package `npm install -g aip-toolkit`
 2. create a8 project `npx aip-toolkit create <project-name> -c <context-path>`
@@ -23,4 +26,14 @@ npx aip-toolkit create helloworld -c "hello"
 
 ~~~sh
 npx aip-toolkit module helloworld
+~~~
+
+### Rich Client Project
+
+1. install the package `npm install -g aip-toolkit`
+2. create a8 project `npx aip-toolkit rich <project-name>`
+3. cd project-name directory and run `./buildandpublish.sh`
+
+~~~sh
+npx aip-toolkit rich helloworld
 ~~~
--- index.js
+++ index.js
@@ -4,30 +4,43 @@ const program = require("commander");
 const creator = require("./lib/creator");
 
 function main() {
-    program.version('0.1.8');
-    program.usage('create app-name [options]');
-    program.command('create <app-name>')
-        .option('-c, --context <context>', 'front-end context path')
-        .option('-a, --author <author>', 'front-end project creator')
-        .option('-e, --email <email>', 'front-end project creator email')
-        .option('-v, --version <version>', 'front-end project version')
-        .option('-p, --port <port>', 'front-end project listene port')
-        .action(function (appName, options) {
-            creator(appName, options, false);
-        });
+  program.version("0.1.8");
+  program.usage("create app-name [options]");
+  program
+    .command("create <app-name>")
+    .option("-c, --context <context>", "front-end context path")
+    .option("-a, --author <author>", "front-end project creator")
+    .option("-e, --email <email>", "front-end project creator email")
+    .option("-v, --version <version>", "front-end project version")
+    .option("-p, --port <port>", "front-end project listene port")
+    .action(function (appName, options) {
+      creator(appName, options, 1);
+    });
 
-    program.command('module <module-name>')
-        .option('-a, --author <author>', 'front-end project creator')
-        .option('-e, --email <email>', 'front-end project creator email')
-        .option('-v, --version <version>', 'front-end project version')
-        .action(function (appName, options) {
-            creator(appName, options, true);
-        });
+  program
+    .command("module <module-name>")
+    .option("-a, --author <author>", "front-end project creator")
+    .option("-e, --email <email>", "front-end project creator email")
+    .option("-v, --version <version>", "front-end project version")
+    .action(function (appName, options) {
+      creator(appName, options, 2);
+    });
 
-    program.description('create a8 project/aip module from base template');
-    program.parse(process.argv);
+  program
+    .command("rich <app-name>")
+    .option("-a, --author <author>", "rich-client project creator")
+    .option("-e, --email <email>", "rich-client project creator email")
+    .option("-v, --version <version>", "rich-client project version")
+    .action(function (appName, options) {
+      creator(appName, options, 3);
+    });
+
+  program.description(
+    "create a8 project/aip-module/rich-client from base template"
+  );
+  program.parse(process.argv);
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
